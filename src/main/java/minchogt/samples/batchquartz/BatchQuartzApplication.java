36d38ce178e1ea4542a21c264ea9394595736092
MinchoGreenT <dldmstjq99@gmail.com>
2024-12-21 16:53:01 +0900

    initial commit

---
+++ src/main/java/minchogt/samples/batchquartz/BatchQuartzApplication.java
@@ -0,0 +1,13 @@
+package minchogt.samples.batchquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchQuartzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minchogt/samples/batchquartz/BatchQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package minchogt.samples.batchquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
